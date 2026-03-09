cd2a09db3f255e3315e43e8321069eb89b2ef123
OisinSD <oisin.s.dillon@gmail.com>
2026-03-09 19:32:26 +0000

    PP-72 Created AccessLog and projectScreenShot Controllers, with endpoints.

---
+++ pp/src/main/java/portfolio/pp/controllers/AccesslogController.java
@@ -0,0 +1,21 @@
+package portfolio.pp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import portfolio.pp.repos.AccessLogRepo;
+import portfolio.pp.models.AccessLog;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accessLogs")
+public class AccesslogController {
+    private final AccessLogRepo accessLogRepo;
+
+    public AccesslogController(AccessLogRepo accessLogRepo){
+        this.accessLogRepo = accessLogRepo;
+    }
+
+    public List<AccessLog> getAllAccessLogsMetaData(){
+        return accessLogRepo.findAll();
+    }
+}

b409b919a934b82ab16de8a4e4b2976743ab2258
OisinSD <oisin.s.dillon@gmail.com>
2026-03-19 15:40:01 +0000

    PP-72 Updated missing API Mapping

--- pp/src/main/java/portfolio/pp/controllers/ProjectSkillController.java
+++ pp/src/main/java/portfolio/pp/controllers/ProjectSkillController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/ProejectSkill")
 public class ProjectSkillController {
 
     private final ProjectSkillRepo projectSkillRepo;
